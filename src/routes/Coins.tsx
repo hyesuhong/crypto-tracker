ccdcd8daddb78f2200011df840a18186df121b01
HyesuHong <honghs95@gmail.com>
2022-05-11 21:38:50 +0900

    find ohter icon api

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -86,7 +86,7 @@ function Coins() {
 						<Coin key={coin.id}>
 							<Link to={`/${coin.id}`} state={{ name: coin.name }}>
 								<Img
-									src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+									src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
 									alt=''
 								/>
 								{coin.name}
